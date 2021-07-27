1118a231dc79979f5fea11fe8d4cb96eb3a7ba90
sangel667 <54800807+sangel667@users.noreply.github.com>
2021-07-27 10:41:26 +0100

    Add files via upload

---
+++ shops/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import generic
+from django.contrib.gis.geos import fromstr, Point
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
+
+longitude = -80.191788
+latitude = 25.761681
+
+user_location = Point(longitude,latitude, srid=4326)                              
+
+class Home(generic.ListView):
+    model = Shop
+    context_object_name = 'shops'
+    queryset = Shop.objects.annotate(distance=Distance('location', 
+    user_location)
+    ).order_by('distance')[0:6]
+    template_name = 'shops/index.html'
+
+home = Home.as_view()
