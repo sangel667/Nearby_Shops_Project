dc6b3c9de4ecc8f61e9dee2bcf21f3779149eb6d
sangel667 <54800807+sangel667@users.noreply.github.com>
2021-07-27 10:27:02 +0100

    Delete shops directory

--- shops/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib.gis.admin import OSMGeoAdmin
-from .models import Shop
-
-@admin.register(Shop)
-class ShopAdmin(OSMGeoAdmin):
-    list_display = ('name', 'location')
